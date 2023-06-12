f8123626fd78294313ec41d8f7b822d6b85b8fca
JimmyDeng <121dengpeng@163.com>
2023-06-12 19:40:10 +0800

    small fix

---
+++ marlpo/modules/__init__.py
@@ -0,0 +1,7 @@
+from .ar_utils import *
+from .attention import *
+from .gae import gae_trace, masked_normalization
+from .mlp import mlp
+from .rnn import AutoResetRNN
+from .recurrent_backbone import RecurrentBackbone
+from .popart import PopArtValueHead

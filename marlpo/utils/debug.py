526f76d95e945350455db09f44413644fa58e955
JimmyDeng <121dengpeng@163.com>
2023-06-25 21:17:59 +0800

    update debug.py

--- marlpo/utils/debug.py
+++ marlpo/utils/debug.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Dict
 import rich
 from rich import print, inspect
 from rich.panel import Panel
@@ -15,24 +15,6 @@ import yaml
 
 from ray.rllib.utils import try_import_torch
 torch, _ = try_import_torch()
-
-def is_tensor_type(data):
-    if isinstance(data, torch.Tensor) or \
-       isinstance(data, np.ndarray):
-        return True
-    else:
-        return False
-
-def refine_tensor_type(data):
-    if isinstance(data, torch.Tensor):
-        if len(data.shape) == 0:
-            return str(data)
-        elif len(data.shape) == 1 and data.shape[0] == 1:
-            return str(data)
-        else:
-            return data.shape
-    elif isinstance(data, np.ndarray):
-        return type(data).__name__ + '.shape=' + str(data.shape)
 
 class SafeFallbackEncoder(json.JSONEncoder):
     def __init__(self, nan_str="null", **kwargs):
@@ -70,15 +52,129 @@ def pretty_print(result):
     cleaned = json.dumps(out, cls=SafeFallbackEncoder)
     return yaml.safe_dump(json.loads(cleaned), default_flow_style=False)
 
-def dict_to_str(a_dict: dict, indent=2, use_json=False, raw_output=False):
+
+class ValueWrapper:
+    SEPARATOR = 'SEPARATOR'
+    DATA = 'DATA'
+
+    def __init__(self, data, raw_tensor):
+        self.raw_data = data 
+        self.type = self.DATA
+        self.str_data = self._convert(data, raw_tensor)
+        
+
+    # wrap the converting logic
+    def _convert(self, data, raw_tensor) -> str:
+        # print('coverting...', data)
+        s = ''
+        if self._is_tensor_type(data):
+            if not raw_tensor:
+                s = self._refine_tensor_type(data)
+            else:
+                s = '\n' + str(data)
+        elif data == '-' or data == '--' or data == '---':
+            self.type = self.SEPARATOR
+            s = data
+        else:
+            s = str(data)
+
+        return s
+
+
+    def _is_tensor_type(self, data):
+        if isinstance(data, torch.Tensor) or \
+        isinstance(data, np.ndarray):
+            return True
+        else:
+            return False
+
+    def _refine_tensor_type(self, data):
+        if isinstance(data, torch.Tensor):
+            if len(data.shape) == 0:
+                return str(data)
+            elif len(data.shape) == 1 and data.shape[0] == 1:
+                return str(data)
+            else:
+                return str(data.shape)
+        elif isinstance(data, np.ndarray):
+            return type(data).__name__ + '.shape=' + str(data.shape)
+    
+    def __str__(self) -> str:
+        return self.str_data
+
+    def __len__(self):
+        return len(self.str_data)
+
+
+def process_value(
+        a_dict: dict, 
+        indent=2, 
+        use_json=False, 
+        raw_tensor=False, 
+        align=False
+    ):
+    new_dict = {}
+
+    for k, v in a_dict.items():
+        if isinstance(v, dict):
+            new_dict[k] = process_value(v, raw_tensor)
+        else:
+            new_dict[k] = ValueWrapper(v, raw_tensor)
+
+    return new_dict
+
+
+
+
+
+
+def dict_to_str(
+        a_dict: dict, 
+        indent=2, 
+        use_json=False, 
+        raw_tensor=False, 
+        align=False
+    ):
+    """Convert a dict to a string that translate every key-value pair and its
+    sub-dict key-value pairs into separate rows. 
+
+    If a key-value is '-': '-', it means a dash-line saparator, and it will be
+    converted to a '-'*N line, the length will be inferred automatically.
+
+    Args:
+        a_dict: the input dict to transfer to a str.
+        indent: number of space before each sub-row.
+        use_json: whether to use json encoder.
+        raw_tensor: output raw numbers for numpy and tensor if True; otherwize
+            only output types of them.
+        align: whether to align each key's colon.
+    """
     # 1. hand-written conversion
+    def get_max_key_len(d):
+        max_len = 0
+        for k in d:
+            max_len = max(len(k), max_len)
+        return max_len
+
+    def get_max_kv_len(d):
+        max_len = 0
+        for k, v in d.items():
+            if isinstance(v, dict):
+                sub_len = get_max_kv_len(v) + indent
+                max_len = max(max_len, sub_len)
+            else:
+                max_len = max(max_len, len(k) + 2 + len(v))
+        return max_len
+
+    max_key_len = get_max_key_len(a_dict)
+
     if not use_json:
-        need_prefix = False
+        newline_prefix = False
         res_str = ''
         for k, v in a_dict.items():
-            if not need_prefix:
+            if not newline_prefix:
                 prefix = ''
-                need_prefix = True
+                newline_prefix = True
             else:
                 prefix = '\n'
 
@@ -89,13 +185,22 @@ def dict_to_str(a_dict: dict, indent=2, use_json=False, raw_output=False):
                 for l in vv.split('\n'):
                     v.append(' '*indent + l)
                 v = '\n' + '\n'.join(v)
-            if is_tensor_type(v):
-                if not raw_output:
+            elif is_tensor_type(v):
+                if not raw_tensor:
                     v = refine_tensor_type(v)
                 else:
                     v = '\n' + str(v)
-                
-            res_str += prefix + k + ': ' f'{v}'
+            
+            if k == v == '-':
+                max_line_len = get_max_kv_len(a_dict)
+                res_str += '-'*max_line_len
+
+            else:
+                if align:
+                    key_str = (k+': ').ljust(max_key_len+2)
+                else:
+                    key_str = k + ': '
+                res_str += prefix + key_str + f'{v}'
 
     # 2. use json dump
     else:
@@ -104,18 +209,126 @@ def dict_to_str(a_dict: dict, indent=2, use_json=False, raw_output=False):
     return res_str
 
 
+def get_max_key_len(d) -> int:
+    """Return a maximun key length of a dict"""
+    max_len = 0
+    for k in d:
+        max_len = max(len(k), max_len)
+    return max_len
+
+
+def get_max_kv_len(d: Dict[str, ValueWrapper], indent, align):
+    """ Return a maximum sum of length of key-value pair in a dict.
+        Including colon ': '.
+    """
+    # for in-line indent, other shorter keys will be adjusted to this max_len
+    max_key_len = get_max_key_len(d)
+    # compute indent len for NEW line! Not in-line!
+    cur_indent = max(max_key_len+2, indent) if align else indent
+    max_len = 0
+    for k, v in d.items():
+        # for dict, it will be put in a new-line,
+        # so the length for this dict will be indent + the dict's max_line
+        if isinstance(v, dict):
+            sub_len = get_max_kv_len(v, indent, align) + cur_indent
+            max_len = max(max_len, sub_len)
+        # for normal data, the key should be extended,
+        # so the length will be the extended key length + value length  
+        else:
+            max_len = max(max_len, max_key_len+2+len(v))
+    return max_len
+
+def new_dict_to_str(
+        a_dict: Dict[str, ValueWrapper], 
+        indent=2, 
+        use_json=False, 
+        raw_tensor=False, 
+        align=True,
+    ):
+    """Convert a dict to a string that translate every key-value pair and its
+    sub-dict key-value pairs into separate rows. 
+
+    If a key-value is '-': '-', it means a dash-line saparator, and it will be
+    converted to a '-'*N line, the length will be inferred automatically.
+
+    Args:
+        a_dict: the input dict to transfer to a str.
+        indent: number of space before each sub-row.
+        use_json: whether to use json encoder.
+        raw_tensor: output raw numbers for numpy and tensor if True; otherwize
+            only output types of them.
+        align: whether to align each key's colon.
+    """
+
+
+    max_key_len = get_max_key_len(a_dict)  
+    cur_indent = max(max_key_len, indent) if align else indent
+
+    # 1. hand-written conversion
+    if not use_json:
+        newline_prefix = False
+        res_str = ''
+        for k, v in a_dict.items():
+            if not newline_prefix:
+                prefix = ''
+                newline_prefix = True
+            else:
+                prefix = '\n'
+
+            # 1. dict
+            if isinstance(v, dict):
+                vv = new_dict_to_str(v, align=align)
+                shift = cur_indent + 2
+                l = [' '*shift + line for line in vv.split('\n')]
+                v_str = '\n' + '\n'.join(l)
+                k_str = k
+                res_str += prefix + k_str + ': ' + v_str
+
+            # 2. '---'
+            elif v.type == ValueWrapper.SEPARATOR:
+                max_line_len = get_max_kv_len(a_dict, indent=cur_indent, align=align)
+                k_str = '-'*(max_key_len+1)
+                v_str = '-'*(max_line_len-len(k_str)-1)
+                res_str += prefix + k_str + ' ' + v_str
+
+            # 3. normal in-line data
+            elif v.type == ValueWrapper.DATA:
+                if align: 
+                    k_str = (k+': ').ljust(max_key_len+2)
+                else:
+                    k_str = k + ': '
+                res_str += prefix + k_str + str(v)
+               
+            else:
+                raise NotImplementedError
+
+
+    # 2. use json dump
+    else:
+        res_str = json.dumps(a_dict, indent=indent)
+
+    return res_str
+
+
+def new(a_dict):
+    new_dict = process_value(a_dict)
+    return new_dict_to_str(new_dict)
+
+
+
 
 def log(title: str = None, obj=None, docs=False):
     inspect(obj=obj, title=title, docs=docs)
 
 
-def printPanel(msg, title=None, raw_output=False, **kwargs):
+def printPanel(msg, title=None, raw_output=False, align=True, **kwargs):
     if isinstance(msg, str):
         pass
     elif isinstance(msg, int):
         msg = str(msg)
     elif isinstance(msg, dict):
-        msg = dict_to_str(msg, raw_output=raw_output)
+        new_dict = process_value(msg)
+        msg = new_dict_to_str(new_dict, raw_tensor=raw_output, align=align)
     else:
         print(f'[Warning] utils.py:printPanel -- msg with type {type(msg)}')
 
@@ -124,29 +337,81 @@ def printPanel(msg, title=None, raw_output=False, **kwargs):
 
 if __name__ == '__main__':
     import math
-
-    array = np.random.random(size=(2, 3))
-    t = torch.randn(2,3,4)
     t2 = torch.tensor(True)
+
+
+    d0 = {
+        'x': torch.randn(2,3,4),
+        '-': '-',
+        'xx': math.e,
+    }
+
+    d1 = {
+        'a': np.random.random(size=(2, 3)),
+        '-': '-',
+        'aa': 3.14,
+    }
 
     data = {
         'name': 'John',
         'age': 30,
-        'city': t2
+        '-': '-',
+        'data_0': 'DATA0',
+        'data_1': d0,
+        'data_2': {
+            'd': 'DDD',
+            '-': '-',
+            'ddd': d1
+        }
+
     }
 
-    d1 = dict(
-        x=t,
-        y=math.e,
-        z=data,
-    )
-    d2 = dict(
-        a=array,
-        b='hello',
-        c=3.14,
-        d=d1
-    )
     # print(dict_to_str(d2))
     # print(dict_to_str(d2, use_json=True))
 
-    printPanel(d2, title='title', raw_output=True)
+    printPanel(data, title='title',align=True, raw_output=False)
+    # s = new(data)
+    # print(s)
+
+    def get_max_kv_len(d: Dict[str, ValueWrapper]):
+        max_len = 0
+        for k, v in d.items():
+            if isinstance(v, dict):
+                sub_len = get_max_kv_len(v) + 2
+                max_len = max(max_len, sub_len)
+            else:
+                max_len = max(max_len, len(k) + 2 + len(v))
+        return max_len
+    
+    d = process_value(data)
+    print(get_max_kv_len(d))
+
+
+'''
+╭────────── title ──────────╮
+│ name:   John              │
+│ age:    30                │
+│ ----------                │
+│ data_0: DATA0             │
+│ data_1:                   │
+│   a: ndarray.shape=(2, 3) │
+│   aa: 3.14                │
+╰───────────────────────────╯
+
+╭─────────── title ───────────╮
+│ name:   John                │
+│ age:    30                  │
+│ --------------------------- │
+│ data_0: DATA0               │
+│ data_1:                     │
+│   x: torch.Size([2, 3, 4])  │
+│   y: 2.718281828459045      │
+│ data_2:                     │
+│   ddd: DDD                  │
+│   ------------------------- │
+│   d:                        │
+│     a: ndarray.shape=(2, 3) │
+│     ----------------------- │
+│     aa: 3.14                │
+╰─────────────────────────────╯
+'''
